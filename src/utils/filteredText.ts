4bf5244514d8741f6be9a823f5e0d6e0a252169c
Sava <86553293+TheHix@users.noreply.github.com>
2022-12-21 11:46:20 +0300

    updated text filtering

--- src/utils/filteredText.ts
+++ src/utils/filteredText.ts
@@ -1,19 +1,30 @@
 import { userSettings } from "./../constants/renderData";
 
-const getFilteredText = (text: string, regExp: RegExp, replacementExp: string, userSetting: string) => {
-  if (!text || !regExp) return "";
+const getFilteredText = (
+  text: string,
+  regularExp: string,
+  replacementExp: string,
+  userSetting: string
+): string | undefined => {
+  if (!text || !regularExp) return "";
 
   if (!userSettings.includes(userSetting)) {
     throw new Error("Неизвестная пользовательская настройка");
   }
 
+  const regExp = RegExp(regularExp, "gi");
+
   if (userSetting === userSettings[0]) {
     return text.replace(regExp, replacementExp);
-  } else if (userSetting === userSettings[1]) {
+  }
+  if (userSetting === userSettings[1]) {
     return text.match(regExp)?.join("");
-  } else if (userSetting === userSettings[2]) {
+  }
+  if (userSetting === userSettings[2]) {
     return text.match(regExp)?.join("").replace(regExp, replacementExp);
   }
+
+  return "";
 };
 
 export default getFilteredText;
