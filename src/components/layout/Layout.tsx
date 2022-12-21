f101c3f35b4aa5702755cd8aba6d86a1992a7485
Sava <86553293+TheHix@users.noreply.github.com>
2022-12-21 11:47:17 +0300

    added the ability to change the theme

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,18 +1,50 @@
-import React, { FC, ReactNode } from "react";
+import React, { FC, ReactNode, useCallback, useMemo, useState } from "react";
+import Guide from "../shared/guide/Guide";
 import Header from "../shared/header/Header";
 import styles from "./Layout.module.css";
+
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+import { localStorage } from "../../services/localStorage";
 
 interface LayoutProps {
   children: ReactNode;
 }
 
 const Layout: FC<LayoutProps> = ({ children }) => {
+  const [mode, setMode] = useState<"light" | "dark">(() => localStorage.getTheme());
+
+  const toggleColorMode = useCallback(() => {
+    if (mode === "light") {
+      setMode("dark");
+      localStorage.setTheme("dark");
+    } else {
+      setMode("light");
+      localStorage.setTheme("light");
+    }
+  }, [mode]);
+
+  const theme = useMemo(
+    () =>
+      createTheme({
+        palette: {
+          mode,
+        },
+      }),
+    [mode]
+  );
+
   return (
-    <div className={styles.wrapper}>
-      <Header />
-      {children}
-    </div>
-  ); 
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+
+      <div className={styles.wrapper}>
+        <Header toggleColorMode={toggleColorMode} />
+        {children}
+        <Guide />
+      </div>
+    </ThemeProvider>
+  );
 };
 
 export default Layout;
--- src/components/shared/header/Header.tsx
+++ src/components/shared/header/Header.tsx
@@ -1,14 +1,30 @@
 import React, { FC } from "react";
-import { AppBar, Typography, Toolbar, Container } from "@mui/material";
-import styles from "./Header.module.css";
 
-const Header: FC = () => {
-  //TODO: сделать переход к истории (?)
+import { AppBar, Typography, Toolbar } from "@mui/material";
+import { useTheme } from "@mui/material/styles";
+import Brightness4Icon from "@mui/icons-material/Brightness4";
+import Brightness7Icon from "@mui/icons-material/Brightness7";
+import IconButton from "@mui/material/IconButton";
+
+interface HeaderProps {
+  toggleColorMode: () => void;
+}
+
+const Header: FC<HeaderProps> = ({ toggleColorMode }) => {
+  const theme = useTheme();
+
   return (
     <AppBar position="static">
-      <Toolbar variant="dense">
+      <Toolbar variant="dense" sx={{ display: "flex", justifyContent: "space-between" }}>
         <Typography variant="h6" color="inherit" component="div">
           Тестер регулярных выражений
+        </Typography>
+
+        <Typography sx={{ display: "flex", alignItems: "center" }} variant="h6" color="inherit" component="div">
+          {theme.palette.mode === "dark" ? "темная" : "светлая"} тема
+          <IconButton sx={{ ml: 1 }} onClick={toggleColorMode} color="inherit">
+            {theme.palette.mode === "dark" ? <Brightness7Icon /> : <Brightness4Icon />}
+          </IconButton>
         </Typography>
       </Toolbar>
     </AppBar>
